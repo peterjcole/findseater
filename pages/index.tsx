4b4737988eabab7b34c39b1596ac366103517486
Tyler Crammond <tylerchc92@gmail.com>
2022-07-15 22:02:01 +0100

    ✨ Use last known stations from cookie when visiting index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
-import type { NextPage } from 'next'
+import { getCookie } from 'cookies-next'
+import type { GetServerSideProps, GetServerSidePropsContext, NextPage } from 'next'
 
 const Index: NextPage = () => {
   return <></>
+}
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const [origin, destination] = loadStations(context)
+
+  return {
+    redirect: {
+      permanent: false,
+      destination: `/${origin}/${destination}`,
+    },
+    props: {},
+  };
+}
+
+function loadStations(context: GetServerSidePropsContext) {
+  const stations = getCookie('stations', context)
+  if (typeof stations === 'string' && !!stations.match(/^[A-Z]+\/[A-Z]+$/)) {
+    return stations.split('/')
+  }
+  return ['CHX', 'HGS']
 }
 
 export default Index
